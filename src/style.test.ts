50d20deb54585d5b5ad1ce4ee656931ac18a4ec1
Soeren Walls <SWalls@users.noreply.github.com>
2021-10-11 06:24:49 +0000

    fix: magic number in style test

--- src/style.test.ts
+++ src/style.test.ts
@@ -1,9 +1,11 @@
 import * as Style from './style';
 
+const MINIMUM_COLORS_LENGTH = 1;
+
 describe('style', () => {
   describe('COLORS', () => {
     it('has at least two colors', () => {
-      expect(Style.COLORS.length).toBeGreaterThan(1);
+      expect(Style.COLORS.length).toBeGreaterThan(MINIMUM_COLORS_LENGTH);
     });
   });
 });
