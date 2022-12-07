242815a3a94ab0a865c3d44d3491041360adc5c0
soeren <6632279+DinoSoeren@users.noreply.github.com>
2022-12-07 09:11:50 +0000

    Fix mathUtils tests

--- src/utils/mathUtils.test.ts
+++ src/utils/mathUtils.test.ts
@@ -1,5 +1,3 @@
-/*eslint no-magic-numbers: [0]*/
-/*eslint @typescript-eslint/no-magic-numbers: [0]*/
 import * as MathUtils from './mathUtils';
 
 describe('mathUtils', () => {
@@ -10,30 +8,37 @@ describe('mathUtils', () => {
   afterEach(() => {
     jest.spyOn(global.Math, 'random').mockRestore();
   });
+
   describe('medianValue', () => {
     it('returns expected median value with even length', () => {
       expect(MathUtils.medianValue([6, 4, 2, 1])).toBe(3);
     });
+
     it('returns expected median value with odd length', () => {
       expect(MathUtils.medianValue([6, 2, 1])).toBe(2);
     });
   });
+
   describe('randomBetween', () => {
     it('returns expected "random" number', () => {
-      expect(MathUtils.randomBetween(4, 6)).toBe(5);
+      expect(MathUtils.randomBetween({min: 4, max: 6})).toBe(5);
     });
+
     it('returns expected "random" number', () => {
-      expect(MathUtils.randomBetween(4, 5)).toBe(4.5);
+      expect(MathUtils.randomBetween({min: 4, max: 5})).toBe(4.5);
     });
   });
+
   describe('randomIntBetween', () => {
     it('returns expected "random" integer', () => {
-      expect(MathUtils.randomIntBetween(3, 6)).toBe(4);
+      expect(MathUtils.randomIntBetween({min: 3, max: 6})).toBe(4);
     });
+
     it('returns expected "random" integer floored', () => {
-      expect(MathUtils.randomIntBetween(4, 5)).toBe(4);
+      expect(MathUtils.randomIntBetween({min: 4, max: 5})).toBe(4);
     });
   });
+
   describe('randomSelection', () => {
     it('returns expected "random" element', () => {
       expect(MathUtils.randomSelection([8, 9, 1, 7])).toBe(1);
--- src/utils/timeUtils.test.ts
+++ src/utils/timeUtils.test.ts
@@ -1,5 +1,3 @@
-/*eslint no-magic-numbers: [0]*/
-/*eslint @typescript-eslint/no-magic-numbers: [0]*/
 import * as TimeUtils from './timeUtils';
 
 describe('timeUtils', () => {
@@ -7,20 +5,25 @@ describe('timeUtils', () => {
     it('returns expected Ms duration given valid string inputs', () => {
       expect(TimeUtils.getSubtimeInputAsMs('03', '4', '21')).toBe(184021);
     });
+
     it('returns expected Ms duration given valid number inputs', () => {
       expect(TimeUtils.getSubtimeInputAsMs(2, 5, 29)).toBe(125029);
     });
   });
+
   describe('formatSubtimeInput', () => {
     it('returns formatted 2-digit subtime given valid string inputs', () => {
       expect(TimeUtils.formatSubtimeInput('6', '60')).toBe('06');
     });
+
     it('returns formatted 3-digit subtime given overflow string input', () => {
       expect(TimeUtils.formatSubtimeInput('66', '60', 3)).toBe('060');
     });
+
     it('returns formatted 3-digit subtime given valid number inputs', () => {
       expect(TimeUtils.formatSubtimeInput(92, 1000, 3)).toBe('092');
     });
+
     it('returns formatted 3-digit subtime given overflow number input', () => {
       expect(TimeUtils.formatSubtimeInput(1124971, 1000, 3)).toBe('112');
     });
